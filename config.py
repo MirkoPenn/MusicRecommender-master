ef066a27eec583cee3016987d28c132cf4e89797
Mirko <mirko@pennone.org>
2020-12-08 12:47:47 +0100

    tensorflow

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import utils
 
 # Files and extensions
-DATA_DIR = 'data/'
+DATA_DIR = 'dlrs-data/'
 TEXT_DIR = DATA_DIR + 'text/'
 AUDIO_DIR = DATA_DIR + 'audio/'
 AUDIOFILES_DIR = AUDIO_DIR + 'audio_files/'
